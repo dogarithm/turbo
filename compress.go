bf2ed8b4bb94e60cc3385e76e25ae1c3652810ef
John Pongsajapan <foobar@gmail.com>
2023-10-22 11:31:42 +0700

    less copies

--- compress.go
+++ compress.go
@@ -38,15 +38,12 @@ func Compress(img *Image, params CompressParams) ([]byte, error) {
 	res := C.tjCompress2(encoder, (*C.uchar)(&img.Pixels[0]), C.int(img.Width), C.int(img.Stride), C.int(img.Height), C.int(params.PixelFormat),
 		&outBuf, &outBufSize, C.int(params.Sampling), C.int(params.Quality), C.int(params.Flags))
 
-	var enc []byte
 	err := makeError(encoder, res)
-	if outBuf != nil {
-		enc = C.GoBytes(unsafe.Pointer(outBuf), C.int(outBufSize))
-		C.tjFree(outBuf)
-	}
 
 	if err != nil {
+		C.tjFree(outBuf)
 		return nil, err
 	}
-	return enc, nil
+
+	return unsafe.Slice((*byte)(unsafe.Pointer(outBuf)), outBufSize), nil
 }
